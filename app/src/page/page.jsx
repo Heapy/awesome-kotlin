a0cd959a445f120cb3fedd637d57347782c27455
Ruslan Ibragimov <theliveperson@gmail.com>
2016-02-05 12:27:04 +0300

    Basic search implementation.

--- app/src/page/page.jsx
+++ app/src/page/page.jsx
@@ -5,6 +5,23 @@ import Search from '../search/search.jsx';
 import Category from '../category/category.jsx';
 
 export default class Page extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {data: this.props.categories};
+    }
+
+    filterData(data, value) {
+        return data.filter(it => it.name === value);
+    }
+
+    onSearchValueChanged(value) {
+        if (value) {
+            this.setState({data: this.filterData(this.props.categories, value)});
+        } else {
+            this.setState({data: this.props.categories});
+        }
+    }
+
     render() {
         return (
             <div>
@@ -13,8 +30,8 @@ export default class Page extends React.Component {
                          src={require('./forkme_right_white_ffffff.png')}
                          alt="Fork me on GitHub"/></a>
                 <Head />
-                <Search />
-                {this.props.categories.map((category, i) => {
+                <Search onChange={this.onSearchValueChanged.bind(this)}  />
+                {this.state.data.map((category, i) => {
                     return <Category category={category} key={i}/>;
                 })}
             </div>
