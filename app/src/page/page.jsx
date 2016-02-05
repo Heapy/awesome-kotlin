41a142ca3f4acbd995a33f3283e691e338964b06
Ruslan Ibragimov <theliveperson@gmail.com>
2016-02-05 11:38:35 +0300

    Added fork-me button.

--- app/src/page/page.jsx
+++ app/src/page/page.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './page.less';
 import Head from '../head/head.jsx';
 import Search from '../search/search.jsx';
 import Category from '../category/category.jsx';
@@ -7,6 +8,10 @@ export default class Page extends React.Component {
     render() {
         return (
             <div>
+                <a href="https://github.com/JavaBy/awesome-kotlin">
+                    <img className={styles.page_github_link}
+                         src={require('./forkme_right_white_ffffff.png')}
+                         alt="Fork me on GitHub"/></a>
                 <Head />
                 <Search />
                 {this.props.categories.map((category, i) => {
