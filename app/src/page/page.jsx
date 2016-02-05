1c913aaeba3eab670284892ef6c687d2bb1ce80f
Ruslan Ibragimov <theliveperson@gmail.com>
2016-02-05 14:08:02 +0300

    New search algorithm.

--- app/src/page/page.jsx
+++ app/src/page/page.jsx
@@ -4,14 +4,40 @@ import Head from '../head/head.jsx';
 import Search from '../search/search.jsx';
 import Category from '../category/category.jsx';
 
+const toLower = string => string.toLowerCase();
+const matches = (search, text) => {
+    return text && toLower(text).includes(toLower(search));
+};
+
 export default class Page extends React.Component {
     constructor(props) {
         super(props);
         this.state = {data: this.props.categories};
     }
 
-    filterData(data, value) {
-        return data.filter(it => it.name === value);
+    filterData(categories, value) {
+        const clonedCategories = JSON.parse(JSON.stringify(categories));
+        const searchTerm = toLower(value);
+
+        return clonedCategories.filter(category => {
+            category.subcategories = category.subcategories.filter(subcategory => {
+                subcategory.links = subcategory.links.filter(link => {
+                    if (matches(searchTerm, link.name)) {
+                        return true;
+                    } else if (matches(searchTerm, link.desc)) {
+                        return true
+                    } else if (link.tags && link.tags.filter(tag => matches(searchTerm, tag)).length) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                });
+
+                return subcategory.links.length;
+            });
+
+            return category.subcategories.length;
+        });
     }
 
     onSearchValueChanged(value) {
