5508b388c66616810a485dfa0677302e6b690cae
a.labushkia <a.labushkina>
2016-02-06 16:04:38 +0300

    add styles

--- app/src/category/category.jsx
+++ app/src/category/category.jsx
@@ -5,10 +5,9 @@ import List from '../list/list.jsx';
 export default class Category extends React.Component {
     render() {
         return (
-            <section className={`${styles.category} ${styles.category_block1}`}>
+            <section className={`${styles.category} ${styles[`category_block${this.props.number}`]}`}>
                 <div className={styles.category_wrapper}>
-                    <h2>{this.props.category.name}</h2>
-                    <hr/>
+                    <h2 className={styles.category_title}>{this.props.category.name}</h2>
                     <div className={styles.category_wrapper_lists}>
                         {this.props.category.subcategories.map((subcategory, i) => {
                             return <List links={subcategory} key={i}/>;
