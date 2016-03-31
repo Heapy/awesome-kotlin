712c488f3315eff2cfcc5a40c3ca052834282b23
Ibragimov Ruslan <r.ibragimov@itransition.com>
2016-03-31 14:09:19 +0300

    List ordering #28. Sort by star count.

--- app/src/list/list.jsx
+++ app/src/list/list.jsx
@@ -8,9 +8,11 @@ export default class List extends React.Component {
             <section className={styles.list}>
                 <h3 className={styles.list_title}>{this.props.links.name}</h3>
                 <ul className={styles.list_list}>
-                    {this.props.links.links.map((link, i) => {
-                        return <Listitem link={link} key={i}/>;
-                    })}
+                    {
+                        this.props.links.links
+                            .sort((a, b) => b.star - a.star)
+                            .map((link, i) => <Listitem link={link} key={i}/>)
+                    }
                 </ul>
             </section>
         );
