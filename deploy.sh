b4dffa3fe463ed2119e95429da92e09ea9b29ac7
Ruslan Ibragimov <ruslan@ibragimov.by>
2016-09-03 14:11:03 +0300

    Setup deploy via rsync. Attempt #1.

--- deploy.sh
+++ deploy.sh
@@ -15,22 +15,5 @@ mkdir -p ./dist/articles;
 npm run ci
 cp robots.txt ./dist/
 
-# go to the dist directory and create a *new* Git repo
-cd dist
-git init
-
-# inside this git repo we'll pretend to be a new user
-git config user.name "Travis CI"
-git config user.email "ruslan@ibragimov.by"
-
-# The first and only commit to this new Git repo contains all the
-# files present with the commit message "Deploy to GitHub Pages".
-git add .
-git commit -m "Deploy to GitHub Pages"
-
-# Force push from the current repo's master branch to the remote
-# repo's gh-pages branch. (All previous history on the gh-pages branch
-# will be lost, since we are overwriting it.) We redirect any output to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
-echo "git push"
-git push --force --quiet "https://${GH_TOKEN}@github.com/KotlinBy/awesome-kotlin.git" master:gh-pages
+# sync with remote folder
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist/ deploy@kotlin.link:~/files/kotlin.link
