2994aae857b3aedfde9cb0a49f1e293569c90d9c
Ruslan Ibragimov <ruslan@ibragimov.by>
2016-11-10 22:02:58 +0300

    Focus on search input #63

--- app/src/search/search.jsx
+++ app/src/search/search.jsx
@@ -1,33 +1,39 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import styles from './search.less';
 
 export default class Search extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+  }
 
-    handleChange(event) {
-        this.setState({value: event.target.value});
-        this.props.onChange(event.target.value);
-    }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    this.props.onChange(event.target.value);
+  }
 
-    handleKeyPress(event) {
-        if (event.key === 'Enter') {
-            event.preventDefault();
-            return false;
-        }
+  handleKeyPress(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      return false;
     }
+  }
 
-    render() {
-        return  <section className={styles.search}>
-                    <form className={styles.search_wrapper}>
-                        <input className={styles.search_field}
-                               onKeyPress={this.handleKeyPress}
-                               onChange={this.handleChange.bind(this)}
-                               placeholder="Kotlin is Awesome!"
-                               value={this.state.value}/>
-                    </form>
-                </section>;
-    }
+  componentDidMount() {
+    ReactDOM.findDOMNode(this.refs.search).focus();
+  }
+
+  render() {
+    return <section className={styles.search}>
+      <form className={styles.search_wrapper}>
+        <input className={styles.search_field}
+               ref="search"
+               onKeyPress={this.handleKeyPress}
+               onChange={this.handleChange.bind(this)}
+               placeholder="Type to Filter"
+               value={this.state.value}/>
+      </form>
+    </section>;
+  }
 }
