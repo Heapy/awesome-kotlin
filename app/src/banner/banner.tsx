6373683820fdd756375954e9180735db620fd92e
Ruslan Ibragimov <ruslan@ibragimov.by>
2017-04-18 14:04:16 +0300

    Update link to Kotlin Conf.

--- app/src/banner/banner.tsx
+++ app/src/banner/banner.tsx
@@ -4,7 +4,8 @@ const styles = require("./banner.less");
 
 export function Banner() {
     return (
-        <a className={styles.conference_banner} href="https://kotlinconf.com">
+        <a className={styles.conference_banner}
+           href="https://kotlinconf.com/?utm_source=kotlinlink&utm_medium=web">
             <div className={styles.conference_banner__img}/>
         </a>
     );
