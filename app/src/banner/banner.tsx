2e0d3f7e9e6242c3dd57e610e64225717e088e66
Ibragimov Ruslan <IRus@users.noreply.github.com>
2017-04-20 00:20:08 +0300

    Update banner.tsx

--- app/src/banner/banner.tsx
+++ app/src/banner/banner.tsx
@@ -5,7 +5,7 @@ const styles = require("./banner.less");
 export function Banner() {
     return (
         <a className={styles.conference_banner}
-           href="https://kotlinconf.com/?utm_source=kotlinlink&utm_medium=web">
+           href="https://kotlinconf.com/?utm_source=kotlinlink&utm_medium=web&utm_campaign=superearlybird">
             <div className={styles.conference_banner__img}/>
         </a>
     );
