d8d51de8af3b42d68e40c930a81d0c245a020a31
Ruslan Ibragimov <ruslan@ibragimov.by>
2017-04-23 11:37:51 +0300

    Update to webpack 2.

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from "react-dom";
 import {Root} from "./root";
 import "./style.less"
-import "css!normalize.css"
+import "css-loader!normalize.css"
 
 ReactDOM.render(
     <Root/>,
--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,109 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const autoprefixer = require("autoprefixer");
 
-// Settings
-// export NAME=VALUE
-const NODE_ENV = process.env.NODE_ENV || 'development'; // 'production'
-const SOURCE_MAP = process.env.SOURCE_MAP || 'eval-source-map'; // 'source-map'
+module.exports = function (options = {}) {
+  // Settings
+  // --env.NODE_ENV root --env.SOURCE_MAP source-map ...
+  const NODE_ENV = options.NODE_ENV || "development"; // "production"
+  const SOURCE_MAP = options.SOURCE_MAP || "eval-source-map"; // "source-map"
 
-console.log(`
+  console.log(`
 Build started with following configuration:
 ===========================================
 → NODE_ENV: ${NODE_ENV}
 → SOURCE_MAP: ${SOURCE_MAP}
 `);
 
-const config = {
+  return {
     entry: {
-        vendor: [
-            'babel-polyfill'
-        ],
-        app: [
-            path.resolve(__dirname, 'app', 'src', 'main.tsx')
-        ]
+      vendor: [
+        "babel-polyfill"
+      ],
+      app: [
+        path.resolve(__dirname, "app", "src", "main.tsx")
+      ]
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js?[hash]',
-        publicPath: '/'
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].js?[hash]",
+      publicPath: "/"
     },
     resolve: {
-      extensions: ['', '.tsx', '.js']
+      extensions: [".ts", ".tsx", ".js"]
     },
-    module: {
-        loaders: [{
-            test: /\.json$/,
-            loader: 'json'
-        }, {
-            test: /\.tsx?$/,
-            exclude: /node_modules/,
-            loader: 'ts'
-        }, {
-            test: /\.less$/,
-            loader: 'style!css?modules&localIdentName=[name]_[local]!postcss!less'
-        }, {
-            test: /\.(png|jpg|gif|svg)$/,
-            loader: 'url?limit=32768'
-        }]
-    },
-    postcss: [
-        require('autoprefixer')
-    ],
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'app', 'index.html'),
-            favicon: path.resolve(__dirname, 'app', 'favicon.ico'),
-            hash: true
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify(NODE_ENV)
-            }
-        })
-    ],
+    bail: false,
     devtool: SOURCE_MAP,
+    module: {
+      rules: [{
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "ts-loader"
+      }, {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader",
+          options: {
+            modules: true,
+            localIdentName: "[name]_[local]"
+          }
+        }, {
+          loader: "postcss-loader",
+          options: {
+            plugins: function () {
+              return [
+                autoprefixer
+              ];
+            }
+          }
+        }, {
+          loader: "less-loader"
+        }]
+      }, {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "url-loader",
+        options: {
+          limit: 32768
+        }
+      }]
+    },
+    plugins: createListOfPlugins({NODE_ENV}),
     devServer: {
-        colors: true,
-        inline: false
+      stats: {
+        chunkModules: false,
+        colors: true
+      },
+      historyApiFallback: true,
+      inline: false
     }
+  }
 };
 
-if (NODE_ENV === 'production') {
-    config.plugins = [
-        ...config.plugins,
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            minChunks: 2
-        })
-    ];
-}
+function createListOfPlugins({NODE_ENV}) {
+  const plugins = [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "app", "index.html"),
+      favicon: path.resolve(__dirname, "app", "favicon.ico"),
+      hash: true
+    }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        "NODE_ENV": JSON.stringify(NODE_ENV)
+      }
+    })
+  ];
 
-module.exports = config;
+  if (NODE_ENV === "production") {
+    plugins.push(
+      new webpack.optimize.CommonsChunkPlugin({
+        name: "vendor",
+        minChunks: 2
+      })
+    );
+  }
+
+  return plugins;
+}
