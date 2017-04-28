d0585df7aa4cabcdb6865234e90a10ca9ae51e4a
Ruslan Ibragimov <ruslan@ibragimov.by>
2017-04-28 07:50:24 +0300

    New article.

--- articles/english/2016/Kotlin in Real Projects.kts
+++ articles/english/2016/Kotlin in Real Projects.kts
@@ -1,14 +1,14 @@
 
 import link.kotlin.scripts.Article
-import link.kotlin.scripts.LinkType.*
-import link.kotlin.scripts.LanguageCodes.*
+import link.kotlin.scripts.LanguageCodes.EN
+import link.kotlin.scripts.LinkType.video
 import java.time.LocalDate
 
 // language=Markdown
 val body = """
 
 <iframe width="960" height="480" src="https://www.youtube.com/embed/lpPbCWpBM3I" frameborder="0" allowfullscreen></iframe>
-
+<iframe src="https://player.vimeo.com/video/170761906?title=0&byline=0&portrait=0" width="960" height="480" frameborder="0" allowfullscreen></iframe>
 
 """
 
