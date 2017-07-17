e6910bae33163700a11e81bec6cb7ee02951fa57
Ruslan Ibragimov <ruslan@ibragimov.by>
2017-07-17 15:14:20 +0300

    Update kotlin to 1.1.4-eap-11.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,19 +5,24 @@ import org.gradle.api.tasks.JavaExec
 import org.gradle.script.lang.kotlin.compile
 import org.gradle.script.lang.kotlin.configure
 import org.gradle.script.lang.kotlin.dependencies
+import org.gradle.script.lang.kotlin.extra
 import org.gradle.script.lang.kotlin.repositories
 import org.gradle.script.lang.kotlin.testCompile
 import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 
 buildscript {
+    extra["kotlinVersion"] = "1.1.4-eap-11"
+    extra["commonMarkVersion"] = "0.9.0"
+
     repositories {
         jcenter()
+        maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap-1.1/") }
     }
 
     dependencies {
         classpath("com.github.jengelman.gradle.plugins:shadow:1.2.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.1.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra["kotlinVersion"]}")
     }
 }
 
@@ -41,6 +46,7 @@ configure<ShadowJar>("shadowJar") {
 
 repositories {
     jcenter()
+    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap-1.1/") }
     maven { setUrl("https://dl.bintray.com/heapy/heap") }
 }
 
@@ -49,8 +55,8 @@ configure<KotlinProjectExtension> {
 }
 
 dependencies {
-    compile("org.jetbrains.kotlin:kotlin-stdlib-jre8:1.1.3")
-    compile("org.jetbrains.kotlin:kotlin-reflect:1.1.3")
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jre8:${extra["kotlinVersion"]}")
+    compile("org.jetbrains.kotlin:kotlin-reflect:${extra["kotlinVersion"]}")
     compile("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:0.16")
 
     compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.8.8")
@@ -61,9 +67,9 @@ dependencies {
     compile("org.jsoup:jsoup:1.10.2")
     compile("by.heap.remark:remark-kotlin:1.2.0")
 
-    compile("org.jetbrains.kotlin:kotlin-script-util:1.1.3")
-    compile("com.atlassian.commonmark:commonmark:0.9.0")
-    compile("com.atlassian.commonmark:commonmark-ext-gfm-tables:0.9.0")
+    compile("org.jetbrains.kotlin:kotlin-script-util:${extra["kotlinVersion"]}")
+    compile("com.atlassian.commonmark:commonmark:${extra["commonMarkVersion"]}")
+    compile("com.atlassian.commonmark:commonmark-ext-gfm-tables:${extra["commonMarkVersion"]}")
 
     compile("com.squareup.okhttp3:okhttp:3.5.0")
 
