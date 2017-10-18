3af173645f4cdcaba341efd6d5cbd9835e2aeec2
Ruslan Ibragimov <ruslan@ibragimov.by>
2017-10-19 00:55:51 +0300

    Update gradle, update kotlin version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,16 @@
-
-import org.gradle.api.plugins.ApplicationPluginConvention
-import org.gradle.script.lang.kotlin.compile
-import org.gradle.script.lang.kotlin.configure
-import org.gradle.script.lang.kotlin.dependencies
-import org.gradle.script.lang.kotlin.extra
-import org.gradle.script.lang.kotlin.repositories
-import org.gradle.script.lang.kotlin.testCompile
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.extra
+import org.gradle.kotlin.dsl.kotlin
+import org.gradle.kotlin.dsl.repositories
 import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 
-buildscript {
-    extra["kotlinVersion"] = "1.1.4-eap-11"
-    extra["commonMarkVersion"] = "0.9.0"
+val kotlinVersion = "1.1.51"
+val commonMarkVersion = "0.9.0"
 
-    repositories {
-        jcenter()
-        maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap-1.1/") }
-    }
-
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra["kotlinVersion"]}")
-    }
-}
-
-apply {
-    plugin("kotlin")
-    plugin("application")
+plugins {
+    application
+    kotlin("jvm", "1.1.51")
 }
 
 configure<ApplicationPluginConvention> {
@@ -34,7 +19,6 @@ configure<ApplicationPluginConvention> {
 
 repositories {
     jcenter()
-    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap-1.1/") }
     maven { setUrl("https://dl.bintray.com/heapy/heap") }
 }
 
@@ -43,9 +27,9 @@ configure<KotlinProjectExtension> {
 }
 
 dependencies {
-    compile("org.jetbrains.kotlin:kotlin-stdlib-jre8:${extra["kotlinVersion"]}")
-    compile("org.jetbrains.kotlin:kotlin-reflect:${extra["kotlinVersion"]}")
-    compile("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:0.17")
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jre8:$kotlinVersion")
+    compile("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+    compile("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:0.19.1")
 
     compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.8.8")
     compile("org.slf4j:slf4j-api:1.7.25")
@@ -57,9 +41,9 @@ dependencies {
     compile("org.jsoup:jsoup:1.10.2")
     compile("by.heap.remark:remark-kotlin:1.2.0")
 
-    compile("org.jetbrains.kotlin:kotlin-script-util:${extra["kotlinVersion"]}")
-    compile("com.atlassian.commonmark:commonmark:${extra["commonMarkVersion"]}")
-    compile("com.atlassian.commonmark:commonmark-ext-gfm-tables:${extra["commonMarkVersion"]}")
+    compile("org.jetbrains.kotlin:kotlin-script-util:$kotlinVersion")
+    compile("com.atlassian.commonmark:commonmark:$commonMarkVersion")
+    compile("com.atlassian.commonmark:commonmark-ext-gfm-tables:$commonMarkVersion")
 
     compile("com.squareup.okhttp3:okhttp:3.8.1")
 
