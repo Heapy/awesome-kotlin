5f8403e03376c563f1a42106d0e450a864a49772
Ruslan Ibragimov <ruslan@ibragimov.by>
2017-12-15 21:56:08 +0300

    Additional logging.

--- src/main/kotlin/link/kotlin/scripts/ProjectLinks.kt
+++ src/main/kotlin/link/kotlin/scripts/ProjectLinks.kt
@@ -2,6 +2,7 @@ package link.kotlin.scripts
 
 import link.kotlin.scripts.utils.DefaultScriptCompiler
 import link.kotlin.scripts.utils.ScriptCompiler
+import link.kotlin.scripts.utils.logger
 import java.nio.file.Files
 import java.nio.file.Paths
 
@@ -25,6 +26,15 @@ class ProjectLinks(private val compiler: ScriptCompiler = DefaultScriptCompiler(
     }
 
     private fun linksFromFile(path: String): Category {
-        return compiler.execute(Files.newInputStream(Paths.get("links/$path")))
+        try {
+            return compiler.execute(Files.newInputStream(Paths.get("links/$path")))
+        } catch (e: Exception) {
+            LOGGER.error("Error while processing file {}.", path, e)
+            throw e
+        }
+    }
+
+    companion object {
+        private val LOGGER = logger<ProjectLinks>()
     }
 }
