1327b9068d5f5e039bf5497ad04a9eadc169ea9d
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-05-06 19:56:49 +0300

    Allow building PRs.

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-# check current branch
-if [ 'master' != $TRAVIS_BRANCH ]; then
-    echo "Deploy only on master branch. Current branch: '$branch'.";
-    exit 0;
+echo "TRAVIS_BRANCH=${TRAVIS_BRANCH}"
+echo "TRAVIS_BUILD_DIR=${TRAVIS_BUILD_DIR}"
+echo "TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST}"
+
+# If current branch not "master" or it's PR
+if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    echo "Clear and re-create the dist directory...";
+    rm -rf dist || exit 1;
+
+    echo "Run kotlin application to generate various data...";
+    ./gradlew installDist
+    AWESOME_KOTLIN_OPTS="-Xmx2g" ./build/install/awesome-kotlin/bin/awesome-kotlin true
+
+    echo "Build React Application...";
+    npm run pack
+
+    echo "Sync with remote folder..."
+    rsync -r --delete-after --quiet "${TRAVIS_BUILD_DIR}/dist/" deploy@kotlin.link:~/files/kotlin.link
+else
+  echo "Deploy only on master branch. Current branch: \"${TRAVIS_BRANCH}\".";
+  exit 0;
 fi
-
-echo "Clear and re-create the dist directory...";
-rm -rf dist || exit 0;
-
-echo "Run kotlin application to generate various data...";
-./gradlew installDist
-AWESOME_KOTLIN_OPTS="-Xmx2g" ./build/install/awesome-kotlin/bin/awesome-kotlin true
-
-echo "Build React Application...";
-npm run pack
-
-echo "Sync with remote folder..."
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist/ deploy@kotlin.link:~/files/kotlin.link
