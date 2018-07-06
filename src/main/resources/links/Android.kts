8ca3f551312d737955ce89f327bc81da7ae32533
Ehsan Mehranvari <ehsunshine@gmail.com>
2018-07-06 15:28:04 +0200

    Added colored-time-selector lib for Android (#287)

--- src/main/resources/links/Android.kts
+++ src/main/resources/links/Android.kts
@@ -253,6 +253,13 @@ category("Android") {
       tags = Tags["diffutil", "googlemaps", "mapbox", "maps", "kotlin", "kotlin-android", "recyclerview", "markers"]
     }
     link {
+      name = "ehsunshine/colored-time-selector"
+      desc = "A smart colored time selector. Users can select just free time with a handy colorful range selector."
+      href = "https://github.com/ehsunshine/colored-time-selector"
+      type = github
+      tags = Tags["selector", "range", "time", "timebar", "kotlin", "kotlin-android", "rangebar", "market"]
+    }
+    link {
       name = "yundom/kache"
       desc = "A lightweight cache library written in Kotlin."
       href = "https://github.com/yundom/kache"
