b1a79172dfc932c51153acad0bf0782c00e76eaa
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-09-21 03:01:31 +0300

    Update node packages.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const autoprefixer = require("autoprefixer");
 
 module.exports = function (options = {}) {
@@ -8,6 +9,8 @@ module.exports = function (options = {}) {
   // --env.NODE_ENV root --env.SOURCE_MAP source-map ...
   const NODE_ENV = options.NODE_ENV || "development"; // "production"
   const SOURCE_MAP = options.SOURCE_MAP || "eval-source-map"; // "source-map"
+
+  const APP_DIR = path.resolve(__dirname, "app");
 
   console.log(`
 Build started with following configuration:
@@ -18,11 +21,8 @@ Build started with following configuration:
 
   return {
     entry: {
-      vendor: [
-        "babel-polyfill"
-      ],
       app: [
-        path.resolve(__dirname, "app", "src", "main.tsx")
+        path.resolve(APP_DIR, "src", "main.tsx")
       ]
     },
     output: {
@@ -70,7 +70,12 @@ Build started with following configuration:
         }
       }]
     },
-    plugins: createListOfPlugins({NODE_ENV}),
+    optimization: {
+      splitChunks: {
+        chunks: "all"
+      }
+    },
+    plugins: createListOfPlugins({NODE_ENV}, APP_DIR),
     devServer: {
       stats: {
         chunkModules: false,
@@ -82,28 +87,17 @@ Build started with following configuration:
   }
 };
 
-function createListOfPlugins({NODE_ENV}) {
-  const plugins = [
+function createListOfPlugins({NODE_ENV}, APP_DIR) {
+  return [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "app", "index.html"),
-      favicon: path.resolve(__dirname, "app", "favicon.ico"),
+      template: path.resolve(APP_DIR, "index.html"),
       hash: true
     }),
+    new CopyWebpackPlugin([{from: path.resolve(APP_DIR, "icons")}]),
     new webpack.DefinePlugin({
       "process.env": {
         "NODE_ENV": JSON.stringify(NODE_ENV)
       }
     })
   ];
-
-  if (NODE_ENV === "production") {
-    plugins.push(
-      new webpack.optimize.CommonsChunkPlugin({
-        name: "vendor",
-        minChunks: 2
-      })
-    );
-  }
-
-  return plugins;
 }
