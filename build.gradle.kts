d6b2644c357daba87c5adb1e4c0ed4d308b6b1ad
Jean-Michel Fayard <jmfayard@gmail.com>
2018-12-16 18:35:20 +0100

    Add and run Gradle buildSrcVersions
    See https://github.com/jmfayard/buildSrcVersions
    
    Have a look at `buildSrc/src/main/kotlin/Versions.kt` for the available updates.
    That includes Kotlin 1.3.11 and Gradle 5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,10 @@ import org.gradle.kotlin.dsl.repositories
 import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 
-val kotlinVersion: String by project
-val commonMarkVersion: String by project
-
 plugins {
     application
-    kotlin("jvm") version "1.2.70"
+    kotlin("jvm") version Versions.org_jetbrains_kotlin_jvm_gradle_plugin
+    id("de.fayard.buildSrcVersions") version Versions.de_fayard_buildsrcversions_gradle_plugin
 }
 
 application {
@@ -28,27 +26,23 @@ repositories {
 }
 
 dependencies {
-    compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
-    compile("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
-    compile("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:0.22.5")
 
-    compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.2")
-    compile("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.9.3")
-    compile("org.slf4j:slf4j-api:1.7.25")
-    compile("ch.qos.logback:logback-classic:1.2.3")
-    compile("io.sentry:sentry-logback:1.6.3")
-
-    compile("com.rometools:rome:1.7.0")
-    compile("com.github.dfabulich:sitemapgen4j:1.0.6")
-    compile("org.jsoup:jsoup:1.10.2")
-    compile("by.heap.remark:remark-kotlin:1.2.0")
-
-    compile("org.jetbrains.kotlin:kotlin-script-util:$kotlinVersion")
-    compile("org.jetbrains.kotlin:kotlin-compiler-embeddable:$kotlinVersion")
-    compile("com.atlassian.commonmark:commonmark:$commonMarkVersion")
-    compile("com.atlassian.commonmark:commonmark-ext-gfm-tables:$commonMarkVersion")
-
-    compile("com.squareup.okhttp3:okhttp:3.8.1")
-
-    testCompile("junit:junit:4.12")
+    compile(Libs.kotlin_stdlib_jdk8)
+    compile(Libs.kotlin_reflect)
+    compile(Libs.kotlinx_coroutines_jdk8) 
+    compile(Libs.jackson_module_kotlin) 
+    compile(Libs.jackson_dataformat_xml) 
+    compile(Libs.slf4j_api) 
+    compile(Libs.logback_classic) 
+    compile(Libs.sentry_logback) 
+    compile(Libs.rome) 
+    compile(Libs.sitemapgen4j) 
+    compile(Libs.jsoup) 
+    compile(Libs.remark_kotlin) 
+    compile(Libs.kotlin_script_util) 
+    compile(Libs.kotlin_compiler_embeddable) 
+    compile(Libs.commonmark) 
+    compile(Libs.commonmark_ext_gfm_tables) 
+    compile(Libs.okhttp) 
+    testCompile(Libs.junit)
 }
