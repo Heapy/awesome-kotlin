d2b184b0b1f6da24f944c04e0b8291119d9c5f68
Ruslan Ibrahimau <ruslan.ibrahimau@askapplications.com>
2020-10-03 02:17:00 +0300

    Fix broken build because of macOS case-insensitive FS :(

--- app-frontend/app/src/root.tsx
+++ app-frontend/app/src/root.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import {Home} from "./pages/home/home";
 import {Route, BrowserRouter} from "react-router-dom";
 import {Switch} from "react-router";
-import {Kugs} from "./pages/kugs/Kugs";
+import {Kugs} from "./pages/kugs/kugs";
 import {Articles} from "./pages/articles/articles";
 
 export function Root() {
