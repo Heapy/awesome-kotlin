d265cfe1e6f78d078f697881ea77b6c549b2b9ef
Ibragimov Ruslan <493656+IRus@users.noreply.github.com>
2020-10-03 18:10:19 +0300

    Fix #117: Add trending section to awesome-kotlin (#610)
    Co-authored-by: Ruslan Ibrahimau <ruslan.ibrahimau@askapplications.com>

---
+++ src/main/kotlin/link/kotlin/scripts/GithubTrending.kt
@@ -0,0 +1,88 @@
+package link.kotlin.scripts
+
+import link.kotlin.scripts.dsl.Category
+import link.kotlin.scripts.dsl.Subcategory
+import link.kotlin.scripts.model.ApplicationConfiguration
+import link.kotlin.scripts.model.Link
+import link.kotlin.scripts.model.default
+import link.kotlin.scripts.utils.Cache
+import link.kotlin.scripts.utils.KotlinObjectMapper
+import link.kotlin.scripts.utils.default
+import org.jsoup.Jsoup
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+private val trending = listOf(
+    "Monthly" to "https://github.com/trending/kotlin?since=monthly",
+    "Weekly" to "https://github.com/trending/kotlin?since=weekly",
+    "Daily" to "https://github.com/trending/kotlin?since=daily"
+)
+
+interface GithubTrending {
+    suspend fun fetch(): Category?
+
+    companion object
+}
+
+private class CachedGithubTrending(
+    private val cache: Cache,
+    private val githubTrending: GithubTrending
+) : GithubTrending {
+    override suspend fun fetch(): Category? {
+        val date = DateTimeFormatter.BASIC_ISO_DATE.format(LocalDate.now())
+        val cacheKey = "github-trending-$date"
+        val cacheValue = cache.get(cacheKey, Category::class)
+
+        return if (cacheValue == null) {
+            val result = githubTrending.fetch()
+            result ?: cache.put(cacheKey, result)
+            result
+        } else cacheValue
+    }
+}
+
+private class JSoupGithubTrending(
+) : GithubTrending {
+    override suspend fun fetch(): Category? {
+        val subcategories = trending.mapNotNull { it.toSubcategory() }.toMutableList()
+
+        return if (subcategories.isNotEmpty()) {
+            Category(
+                name = "Github Trending",
+                subcategories = subcategories
+            )
+        } else null
+    }
+
+    private fun Pair<String, String>.toSubcategory(): Subcategory? {
+        val doc = Jsoup.connect(this.second).get()
+
+        val isAvailable = doc.select(".Box-row").isNotEmpty()
+
+        return if (isAvailable) {
+            val links = doc.select(".Box-row").map {
+                Link(
+                    github = it.select("h1 a").attr("href").removePrefix("/")
+                )
+            }
+
+            return Subcategory(
+                name = this.first,
+                links = links.toMutableList()
+            )
+        } else {
+            null
+        }
+    }
+}
+
+fun GithubTrending.Companion.default(
+    cache: Cache
+): GithubTrending {
+    val jSoupGithubTrending = JSoupGithubTrending()
+
+    return CachedGithubTrending(
+        cache = cache,
+        githubTrending = jSoupGithubTrending
+    )
+}
