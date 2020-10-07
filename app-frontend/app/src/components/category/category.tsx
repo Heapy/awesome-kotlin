a42737f22aaefa0f4a46e765f15c363fff16f0c5
Ruslan Ibrahimau <ruslan.ibrahimau@askapplications.com>
2020-10-07 23:52:28 +0300

    Fix #128: No way to link to a section directly

--- app-frontend/app/src/components/category/category.tsx
+++ app-frontend/app/src/components/category/category.tsx
@@ -13,6 +13,7 @@ export function Category({category}) {
             return (
               <Subcategories
                 key={i}
+                prefix={category.name}
                 subcategory={subcategory}
               />
             );
--- app-frontend/app/src/components/list/list.tsx
+++ app-frontend/app/src/components/list/list.tsx
@@ -10,7 +10,9 @@ export class Subcategories extends React.Component<SubcategoriesProps, Subcatego
 
     return (
       <section ref="subcategory" className={styles.list}>
-        <h3 className={styles.list_title}>{subcategory.name}</h3>
+        <h3 id={getAnchor(this.props.prefix, subcategory.name)} className={styles.list_title}>
+          <a href={`#${getAnchor(this.props.prefix, subcategory.name)}`}>{subcategory.name}</a>
+        </h3>
         <ul className={styles.list_list}>
           {renderLinks(subcategory.links.filter(link => !link.archived && !link.unsupported))}
           {renderLinks(subcategory.links.filter(link => link.unsupported))}
@@ -21,13 +23,21 @@ export class Subcategories extends React.Component<SubcategoriesProps, Subcatego
   }
 }
 
+function getAnchor(prefix: string, suffix: string): string {
+  const normalizedPrefix = prefix.replace(" ", "_").toLowerCase()
+  const normalizedSuffix = suffix.replace(" ", "_").toLowerCase()
+
+  return `${normalizedPrefix}-${normalizedSuffix}`
+}
+
 function renderLinks(links: Link[]) {
   return links.sort((a, b) => b.star - a.star)
     .map((link, i) => <Listitem link={link} key={i}/>)
 }
 
 interface SubcategoriesProps {
-  subcategory: Subcategory;
+  readonly prefix: string;
+  readonly subcategory: Subcategory;
 }
 
 interface SubcategoriesState {
