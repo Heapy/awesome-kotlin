61509345412ea13de151cc9d7c8bad293ddb416c
Ruslan Ibragimov <ruslan@ibragimov.by>
2021-04-18 03:12:49 +0300

    Stop deploying docker image

--- .ci/deploy-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "-> Login to docker hub"
-echo "${DOCKER_TOKEN}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-echo "-> Pushing image heapy/awesome:b${TRAVIS_BUILD_NUMBER} to docker hub"
-docker push heapy/awesome
