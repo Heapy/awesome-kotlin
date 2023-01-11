1a39ba8c85a1ac893df921b9b8ee00cbbadd7a19
Ruslan Ibragimov <ruslan@heapy.io>
2023-01-12 01:51:31 +0300

    Show Kotlin 1.8 versions

--- src/test/kotlin/link/kotlin/scripts/VersionFetcherManualTest.kt
+++ src/test/kotlin/link/kotlin/scripts/VersionFetcherManualTest.kt
@@ -6,6 +6,6 @@ import link.kotlin.scripts.utils.default
 suspend fun main() {
     val client = HttpClient.default()
     val fetcher = KotlinVersionFetcher.default(client)
-    val versions = fetcher.getLatestVersions(listOf("1.6", "1.7"))
+    val versions = fetcher.getLatestVersions(listOf("1.7", "1.8"))
     println(versions)
 }
