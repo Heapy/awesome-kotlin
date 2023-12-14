e42e710fe735f2116fb5e644dd353f807aeae86f
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2023-12-14 11:25:46 +0100

    Add Timefold Solver (open source solver AI)

--- src/main/resources/links/Libraries.awesome.kts
+++ src/main/resources/links/Libraries.awesome.kts
@@ -2021,6 +2021,12 @@ category("Libraries/Frameworks") {
       setTags("data science", "artificial-intelligence", "mathematical-optimization", "constraint-solving")
     }
     link {
+      github = "timefoldai/timefold"
+      desc = "AI solver to optimize planning/scheduling of vehicle routes, employees, maintenance, jobs, etc."
+      href = "https://timefold.ai"
+      setTags("data science", "artificial-intelligence", "mathematical-optimization", "constraint-solving")
+    }
+    link {
       github = "holgerbrandl/kalasim"
       desc = "Discrete event simulator that enables complex, dynamic process models."
       href = "https://www.kalasim.org"
