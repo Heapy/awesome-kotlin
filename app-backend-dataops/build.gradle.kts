e26f7b4519579c3d20d2f01bc7bf3a2fb22a61ac
Ruslan Ibragimov <ruslan@heapy.io>
2024-10-22 09:46:50 +0100

    Dependency refresh

--- app-backend-dataops/build.gradle.kts
+++ app-backend-dataops/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jooq:jooq-codegen:3.19.13")
+    implementation("org.jooq:jooq-codegen:3.19.14")
     implementation("org.flywaydb:flyway-database-postgresql:10.20.0")
     implementation("org.postgresql:postgresql:42.7.4")
     implementation("ch.qos.logback:logback-classic:1.5.11")
